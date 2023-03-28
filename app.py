151e690a29b6313d4905211afe76ecba17260012
Lachlan Paul <lachlanpaul03@gmail.com>
2023-03-28 20:44:42 +1030

    Tidied up design, added a sidebar, and made a page ready to be used for a creation page

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ def home():
     return render_template("/index.html")
 
 
+@app.route("/create")
+def create():
+    return render_template("create.html")
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
